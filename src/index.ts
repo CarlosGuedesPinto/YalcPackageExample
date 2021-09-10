0eca7a16258450d4c2754020b1fdb9b2e4da66b8
carlosguedespinto <cguedes@infraspeak.com>
2021-09-10 14:55:06 +0100

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const Greeter = (name: string) => `Hello Ana Beautiful and Carlos Amazing 2 ${name}`;
+export const Greeter = (name: string) => `Hello Ana Beautiful and Carlos Amazing 3 ${name}`;
